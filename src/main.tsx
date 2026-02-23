16e9a912447c4c1e60cf7af637a8902a29a9ddc6
andrei-filippov <81208085+andrei-filippov@users.noreply.github.com>
2026-02-24 00:04:37 +0700

    24.03 add speedtest

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.tsx'
 import { Toaster } from './components/ui/sonner'
@@ -8,5 +9,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Toaster position="top-right" richColors closeButton />
+    <SpeedInsights />
   </StrictMode>,
 )
